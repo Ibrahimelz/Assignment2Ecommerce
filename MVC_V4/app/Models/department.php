2ffaa6a859f887f83be292950a2ad13617559c1b
lydia230 <143449838+lydia230@users.noreply.github.com>
2025-04-08 22:53:44 -0400

    Replaced incorrect input validation with validInput()

--- MVC_V4/app/Models/department.php
+++ MVC_V4/app/Models/department.php
@@ -1,6 +1,7 @@
 <?php
     namespace models;
-    
+    use core\utils\Utils;
+    require(dirname(__DIR__) . "/core/utils/utils.php");
 
     class Department {
         private $departmentID;
@@ -10,7 +11,7 @@
         public function __construct($name, $departmentID) {
             $this->departmentID = $departmentID;
             if (!empty($name)) {
-                $this->name = self::validateUserInput($name);
+                $this->name = Utils::validateInput($name);
             } else {
                 throw new \InvalidArgumentException("Department name is required!");
             }
